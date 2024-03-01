13c1cf04135f05f4226eccdc179bef2f26d36071
nvere <nicolette.vere@gmail.com>
2024-02-29 21:52:32 -0700

    able to update lat lng

--- main.js
+++ main.js
@@ -1,132 +1,44 @@
 import * as THREE from "three"
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-console.log(THREE)
-const myGlobe = Globe();
-
-function initGlobe(){
-	let main = document.getElementById("main");
-	console.log(main)
-	console.log(myGlobe)
-	myGlobe(main).globeImageUrl("//unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
-}
 
 
 
-function initISS(){
-	const scene = new THREE.Scene();
 
-	const camera = new THREE.PerspectiveCamera(75, 800/ 1000)
-
-	scene.add(camera)
-
-	let hlight = new THREE.AmbientLight(0x404040,100)
-	scene.add(hlight)
-
-	const renderer = new THREE.WebGLRenderer( { alpha: true })
-	renderer.setSize(200, 200)
-
-	
-	document.body.appendChild(renderer.domElement)
-
+export function test(lat,lng){
+  
+	// Gen random data
+	const gData = [{
+	  lat: lat,
+	  lng: lng,
+	  alt: 0.5,
+	  radius: 4,
+	  color: 'grey'}];
 
 	const loader = new GLTFLoader();
 
-	 loader.load('./ISS_stationary.glb', 
+	const world = Globe()(document.getElementById('globeViz'))
+	world.globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+	world.bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+	world.pointOfView({lat:0 , lng:0, altitude: 3.5 })
+	world.customLayerData(gData)
+
+	world.customThreeObjectUpdate((obj, d) => {
+		Object.assign(obj.position, world.getCoords(d.lat, d.lng, d.alt));
+	});
+
+	loader.load('./ISS_stationary.glb', 
 	function ( gltf ) {
 		let ISS = gltf.scene.children[0]
-		//console.log(ISS)
-		
-
-		ISS.scale.set(0.01,0.01,0.01)
-		scene.add( gltf.scene );
-		renderer.render(scene,camera)
-		return ISS
+		  
+  
+		ISS.scale.set(0.1,0.1,0.1,0.1)
+		world.customThreeObject(ISS);
 	}, 
 		undefined, 
 		function ( error ) {
 		console.error( error );
 	});
-	//console.log(x)
-	renderer.render(scene, camera)	
-}
-
-	// const scene = new THREE.Scene();
-
-	// const camera = new THREE.PerspectiveCamera(75, 800/ 1000)
-
-	// scene.add(camera)
-
-	// let hlight = new THREE.AmbientLight(0x404040,100)
-	// scene.add(hlight)
-
-	// const renderer = new THREE.WebGLRenderer( { alpha: true })
-	// renderer.setSize(200, 200)
-
 	
-	//document.body.appendChild(renderer.domElement)
-
-
-
- //let iss = initISS()
-function test(){
-  
-	// Gen random data
-	const N = 1;
-	const gData = [...Array(N).keys()].map(() => ({
-	  lat: 180,
-	  lng: 360,
-	  alt: 1,
-	  radius: 4,
-	  color: 'grey'
-	}));
-	const loader = new GLTFLoader();
-
-	const world = Globe()
-	  (document.getElementById('globeViz'))
-	  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-	  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-	  .pointOfView({ altitude: 3.5 })
-	  .customLayerData(gData)
-	//   .customThreeObject(d => new THREE.Mesh(
-	// 	new THREE.SphereGeometry(d.radius),
-	// 	new THREE.MeshLambertMaterial({ color: d.color })
-	//   ))
-		// .customThreeObject(loader.load( './ISS_stationary.glb', function ( gltf ) {
-
-		// 	world.Scene.add( gltf.scene );
-			
-		
-		// }, undefined, function ( error ) {
-		
-		// 	console.error( error );
-		
-		// } )
-		// )
-	  .customThreeObjectUpdate((obj, d) => {
-		Object.assign(obj.position, world.getCoords(d.lat, d.lng, d.alt));
-	  });
-
-	  loader.load('./ISS_stationary.glb', 
-	  function ( gltf ) {
-		  let ISS = gltf.scene.children[0]
-		  //console.log(ISS)
-		  
-  
-		//   ISS.scale.set(0.01,0.01,0.01)
-		 world.customThreeObject(ISS);
-	  }, 
-		  undefined, 
-		  function ( error ) {
-		  console.error( error );
-	  });
-
-	(function moveSpheres() {
-	  gData.forEach(d => d.lat += 0.2);
-	  world.customLayerData(world.customLayerData());
-	  requestAnimationFrame(moveSpheres);
-	})();
+	
 }
-//test()
-//let iss = initISS()
-//console.log(iss)
-test()
+test(9,8)
--- scripts/issPosition.js
+++ scripts/issPosition.js
@@ -1,10 +1,13 @@
 
+
 function getISSPosition() {
   setInterval(async () => {
     try {
       const response = await axios.get('http://api.open-notify.org/iss-now.json');
       const {latitude, longitude} =  response.data.iss_position;
       console.log('ISS position:', latitude, longitude);
+      test(latitude,longitude)
+
       return {latitude, longitude};
     } catch (error) {
       console.error('Error retrieving ISS position:', error.message);
@@ -12,4 +15,4 @@ function getISSPosition() {
   }, 5000);
 }
 
-// getISSPosition();
+//getISSPosition();
