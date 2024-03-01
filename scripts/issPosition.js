d1e054c849ad7fefd697f9148c02d2430eb948f3
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-02-29 20:24:13 -0800

    add get function for latitude and longitude from ISS API

---
+++ scripts/issPosition.js
@@ -0,0 +1,15 @@
+
+function getISSPosition() {
+  setInterval(async () => {
+    try {
+      const response = await axios.get('http://api.open-notify.org/iss-now.json');
+      const {latitude, longitude} =  response.data.iss_position;
+      console.log('ISS position:', latitude, longitude);
+      return {latitude, longitude};
+    } catch (error) {
+      console.error('Error retrieving ISS position:', error.message);
+    }
+  }, 5000);
+}
+
+// getISSPosition();
